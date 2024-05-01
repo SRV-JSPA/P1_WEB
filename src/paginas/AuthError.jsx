9930b16cd58497ea902be45caa7ef42752ee4641
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-04-30 19:48:32 -0600

    Create AuthError.jsx page

---
+++ src/paginas/AuthError.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function AuthError (){
+  return (
+    <section className='pagina-error' >
+      <div className='centro' >
+        <Link to='/posts/admin' className='btn-error'>Regresar al Login</Link>
+        <h2>Necesita autenticarse primero para acceder a estas fucionalidades</h2>
+      </div>
+    </section>
+  )
+}
+
+export default AuthError
