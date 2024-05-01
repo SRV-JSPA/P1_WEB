99ed94cb4951905feac3a0fbc9517c485810d4ab
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-04-30 19:38:30 -0600

    Create registro.jsx

---
+++ src/paginas/Registro.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+function Registro (){
+  
+  const [usuario, setUsuario] = React.useState('')
+  const [contraseña, setContraseña] = React.useState('')
+
+  const [error, setError] = React.useState('');
+
+ 
+
+  const submithandler = async (e) => {
+    
+    if(!usuario || !contraseña ) {
+      setError("Todos los campos son obligatorios")
+      return;
+    }
+
+    const data = {
+      user: usuario,
+      password: contraseña
+    }
+
+
+    try {
+      const res = await axios.post('http://localhost:3000/users', data)
+      console.log(res);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  
+
+  return (
+    <section className='registro' >
+      <div className='contenedor' >
+        <h2>Registrarse</h2>
+        <form className='form form-registro'>
+        {error && <p className='mensaje-error_form'>{error}</p>}
+          <input type='text' placeholder='Su usuario' name='user' value={usuario.user} onChange={(e) => setUsuario(e.target.value)} autoFocus/>
+          <input type='password' placeholder='Su contraseña' name='contra' value={usuario.contra} onChange={(e) => setContraseña(e.target.value)} />
+          <Link to='/posts/admin' type='submit'  className='btn verde'  onClick={submithandler} >Registrar</Link>
+        </form>
+      <small>Ya tiene una cuenta? <Link to='/posts/admin' >sign in</Link></small>
+      </div>
+    </section>
+  )
+}
+
+export default Registro
