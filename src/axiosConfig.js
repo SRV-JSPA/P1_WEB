3596632948ca61a6277e11c020ddf160dd4fe7ec
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-04-30 20:30:44 -0600

    Create axiosConfig.js

---
+++ src/axiosConfig.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+
+
+const instance = axios.create();
+instance.interceptors.request.use(
+    (config) => {
+      if (!config.url.includes('/login') || config.url.includes('/registro')) {
+        const token = localStorage.getItem('token');
+        if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+        }
+      }
+  
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+  
+
+export default instance
