7c447c9d918d6a2981d6e6254f4e7cab29d3ca06
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-04-30 19:51:03 -0600

    Create Error.jsx page

---
+++ src/paginas/Error.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Error (){
+  return (
+    <section className='pagina-error' >
+      <div className='centro' >
+        <Link to='/posts/Home' className='btn-error'>Regresar a la página principal</Link>
+        <h2>Página no encontrada</h2>
+      </div>
+    </section>
+  )
+}
+
+export default Error
