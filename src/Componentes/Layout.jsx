77be9b1119c35b5987d5f2d2650dd2f7bb048ef1
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2024-04-30 20:36:19 -0600

    Create Layout.jsx component

---
+++ src/Componentes/Layout.jsx
@@ -0,0 +1,18 @@
+import React, { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+import Header from './Header'
+import Footer from './Footer'
+
+function Layout () {
+  return (
+    <>
+        <Header/>
+        <Suspense fallback={<div className="loading">Loading...</div>} >
+          <Outlet/>
+        </Suspense>
+        <Footer/>
+    </>
+  )
+}
+
+export default Layout
